a69c5cf38c1e8062125e2ec01066345c239254cb
JP7FGD <jp7fgd@outlook.com>
2018-05-31 18:12:54 +0900

    update v1.2

--- ImageMean.m
+++ ImageMean.m
@@ -16,29 +16,35 @@ BatchSize = 100; % # of image to process @one time determine based on memory siz
 N = [X_Pixels Y_Pixels BatchSize]; % structure of input data
 BatchCount = Number_Image/BatchSize; % number of division per file
 Number_File = 9; % # of file to analyze
-%cut = 
+cut_Y = 100:650;
+cut_X = 50:500;
 
 %prepare output matrix
-Part_MeanImage = zeros(551,451,BatchCount); % change based on the cut
-MeanImage = zeros(551,451,Number_File);
+Part_MeanImage = zeros(size(cut_Y,2),size(cut_X,2),BatchCount); % change based on the cut
+MeanImage = zeros(size(cut_Y,2),size(cut_X,2),Number_File);
 
 % input file
-file='../../chem_03_cor.dat'; 
-n=1; %temp make another loop with 
+file = 'E:/chem_analysis/chem_%02u_cor.dat'; 
 
+for n=1:Number_File  %temp make another loop with 
+    filename = sprintf(file,n+2);
 %main loop consider to use function hundle 
-for j = 1:BatchCount
-    
-    Image = reshape(loaddat(file,(j-1)*BatchSize*Pixels*2,Pixels*BatchSize),[Y_Pixels,X_Pixels,BatchSize]); %load the image
-    RawImage = permute(Image,[2 1 3]); %permute
-    CutImage = RawImage (100:650,50:500,:); % cut the image if needed
-    Part_MeanImage(:,:,j) = mean(CutImage,3); %obtain a partial mean image
+    for j = 1:BatchCount
+
+        Image = reshape(loaddat(filename,(j-1)*BatchSize*Pixels*2,Pixels*BatchSize),[Y_Pixels,X_Pixels,BatchSize]); %load the image
+        RawImage = permute(Image,[2 1 3]); %permute
+        CutImage = RawImage (cut_Y(1,:),cut_X(1,:),:); % cut the image if needed
+        Part_MeanImage(:,:,j) = mean(CutImage,3); %obtain a partial mean image
+
+    end
+    MeanImage(:,:,n) = mean(Part_MeanImage,3); 
 
 end
-
 %finish up
-
-MeanImage(:,:,n) = mean(Part_MeanImage,3); 
-FileName = ['chem_mean.mat']; % output file name
+FileName = ['E:\chem_analysis\mean\chem_mean.mat']; % output file name
 save(FileName,'MeanImage'); 
+
+%normalization
+Max_Value = max(max(max(MeanImage,[],3))); % find the index
+
 
