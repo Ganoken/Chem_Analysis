b443f36455deb3de4b9ef93e34d1576124c8473d
JP7FGD <jp7fgd@outlook.com>
2018-06-20 00:25:41 +0900

    update

--- ImageMean.m
+++ ImageMean.m
@@ -12,22 +12,23 @@ X_Pixels = 768;
 Y_Pixels = 768;
 Number_Image = 20000; %Total # of image per file
 Pixels = X_Pixels*Y_Pixels; % # of total pixels/image
-BatchSize = 100; % # of image to process @one time determine based on memory size
+BatchSize = 200; % # of image to process @one time determine based on memory size
 N = [X_Pixels Y_Pixels BatchSize]; % structure of input data
 BatchCount = Number_Image/BatchSize; % number of division per file
-Number_File = 1; % # of file to analyze
+Number_File = 9; % # of file to analyze
 cut_Y = 100:650;
-cut_X = 50:500;
+cut_X = 100:600;
 
 %prepare output matrix
 Part_MeanImage = zeros(size(cut_Y,2),size(cut_X,2),BatchCount); % change based on the cut
 MeanImage = zeros(size(cut_Y,2),size(cut_X,2),Number_File);
+Max_Image = zeros(1,Number_File);
 
 % input file
-file = 'I:/chem_analysis/chem_%02u_cor.dat'; 
+file = 'I:/ch0618/ch_%02u_cor.dat'; 
 
 for n=1:Number_File  %temp make another loop with 
-    filename = sprintf(file,n+2);
+    filename = sprintf(file,n+9);
 %main loop consider to use function hundle and clear the memory
     for j = 1:BatchCount
 
@@ -38,16 +39,16 @@ for n=1:Number_File  %temp make another loop with
 
     end
     MeanImage(:,:,n) = mean(Part_MeanImage,3); 
-
+    Max_Image(1,n) = max(max(MeanImage(:,:,n)));
 end
 %finish up
-FileName = ['I:\chem_analysis\mean\chem_mean_t.mat']; % output file name
+FileName = ['I:\ch0618\output\chem_mean_2.mat']; % output file name
 save(FileName,'MeanImage'); 
 
 %normalization
-Max_Value = max(max(max(MeanImage,[],3))); % find the index
+[Max_Value, I] = max(Max_Image); % find the index
 Nor_mean = MeanImage/Max_Value; 
-FileName = ['I:\chem_analysis\mean\chem_nor_mean.mat']; % output file name
+FileName = ['I:\ch0618\output\chem_nor_mean_2.mat']; % output file name
 save(FileName,'Nor_mean'); 
 
 
